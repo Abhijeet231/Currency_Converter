0e45c29f2b7f0ef68def08408e8520aad2d81f04
Ramu kaka <pikupc01@gmail.com>
2026-01-28 10:57:10 +0530

    add: initial files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-amber-100 p-2 sticky top-0">
+        <div className="w-[80%] text-center mx-auto flex justify-around cursor-pointer font-semibold ">
+            <span > Home</span>
+            <span > About</span>
+            <span> Contact</span>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
